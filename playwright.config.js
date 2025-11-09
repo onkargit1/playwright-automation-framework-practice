42ca814551d8782595c52db0e4f4d48e96e112c3
onkarshirodkar <onkar.shirodkar@infobeans.com>
2025-11-09 19:25:36 +0530

    updade mode

--- playwright.config.js
+++ playwright.config.js
@@ -38,7 +38,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
